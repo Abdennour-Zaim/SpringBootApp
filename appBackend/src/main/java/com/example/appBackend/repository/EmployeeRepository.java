9efa1b135f99876c4e1f0101775813ce8a4988d9
Abdennour Zaim <abdennourzaim1999@gmail.com>
2023-05-30 20:40:21 +0100

    first commit

---
+++ appBackend/src/main/java/com/example/appBackend/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.appBackend.repository;
+
+import com.example.appBackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
